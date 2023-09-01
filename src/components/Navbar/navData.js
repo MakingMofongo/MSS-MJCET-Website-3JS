89b35587d8da7d16fce48955e551eb2c5be0b247
clawncher <rasheed3086@gmail.com>
2023-09-02 04:08:30 +0530

    Init

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,36 @@
+export const navLinks = [
+  {
+    label: 'Previous Events',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'About',
+    pathname: '/#details',
+  },
+  {
+    label: 'Team',
+    pathname: '/#member-1',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: '',
+    icon: 'twitter',
+  },
+  {
+    label: 'Figma',
+    url: '',
+    icon: 'figma',
+  },
+  {
+    label: 'Github',
+    url: '',
+    icon: 'github',
+  },
+];
